4a054f09300ce0fc51bc6d61edcfc600a97680e0
Yulia <ytokarchukova@gmail.com>
2018-07-27 18:27:35 +0300

    fix

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('1234'),
+            'remember_token' => '1111'
+        ]);
+    }
+}
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,27 +1,30 @@
 <div class="blog-masthead">
     <nav class="navbar navbar-inverse">
         <div class="container-fluid">
-            
-            @if (Auth::check())            
+
+            @if (Auth::check())
                 <ul class="nav navbar-nav navbar">
-                    <li><a href="http://photogallery.katanji.tk/admin"><span class="glyphicon glyphicon glyphicon-th"></span> Albums</a></li>
+                    <li><a href="{{ url('/admin')  }}"><span class="glyphicon glyphicon glyphicon-th"></span> Albums</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar">
-                    <li><a href="http://photogallery.katanji.tk/create"><span class="glyphicon glyphicon-plus"></span> Add album</a></li>
+                    <li><a href="{{ url('/create') }}"><span class="glyphicon glyphicon-plus"></span> Add album</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a><span class="glyphicon glyphicon-user"></span> {{ Auth::user()->name }}</a></li>
-                    <li><a href="http://photogallery.katanji.tk/logout"><span class="glyphicon glyphicon-log-out"></span> Log Out</a></li>
+                    <li><a href="{{ url('/logout') }}"><span
+                                    class="glyphicon glyphicon-log-out"></span> Log Out</a></li>
                 </ul>
             @else
                 <ul class="nav navbar-nav navbar">
-                    <li><a href="http://photogallery.katanji.tk"><span class="glyphicon glyphicon glyphicon-th"></span> Albums</a></li>
+                    <li><a href="{{ url('/') }}"><span class="glyphicon glyphicon glyphicon-th"></span>
+                            Albums</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="http://photogallery.katanji.tk/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li><a href="{{ url('/login') }}"><span class="glyphicon glyphicon-log-in"></span>
+                            Login</a></li>
                 </ul>
             @endif
-            
+
         </div>
     </nav>
 </div>
