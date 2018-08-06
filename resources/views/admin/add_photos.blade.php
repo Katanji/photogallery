da0230ebf54f955967e34f098ca5f5381184767b
Yulia <ytokarchukova@gmail.com>
2018-08-06 16:26:37 +0300

    max size

--- resources/views/admin/add_photos.blade.php
+++ resources/views/admin/add_photos.blade.php
@@ -10,7 +10,7 @@
     {{ Form::label('name', 'Name:') }}
     {{ Form::text('name', null, array('class' => 'form-control', 'required' => '', 'maxlength' => '32')) }}
 
-    {{ Form::label('featured_image', 'Upload Featured Image(JPEG, PNG, JPG):') }}
+    {{ Form::label('featured_image', 'Upload Featured Image(JPEG, PNG, JPG), max-size - 10Mb :') }}
     {{ Form::file('featured_image') }}
 
     {{ Form::submit('Save', array('class' => 'btn btn-primary')) }}
